f27f16a94efd1b84a5a61b0e1198961924e2e2cc
JamesMcKinley-Portee <54874460+JamesMcKinley-Portee@users.noreply.github.com>
2019-12-08 23:31:33 -0600

    Update MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -1,5 +1,5 @@
 <?php
-echo '<!DOCTYPE html>';
+
 /**
  * ***************************************************************************
  * Title: James McKinley-Portee
@@ -11,18 +11,19 @@ echo '<!DOCTYPE html>';
  * Tested: Fall 2019
  * ****************************************************************************
  */
+?> 
+<!DOCTYPE html>
+<html lang="en-US">
 
-echo '<html lang="en-US">';
+<head>
+<meta charset="utf-8">
+<title>Search</title>
+<link rel="stylesheet" href="main.css">
+</head>
 
-echo '<head>';
-echo '<meta charset="utf-8">';
-echo '<title>Main Menu/title>';
-echo '<link rel="stylesheet" href="main.css">';
-echo '</head>';
+<body> // Page Content
 
-echo '<body>'; // Page Content
-
-
+<?php
 $link_address1 = 'MainMenu.php';
 echo "<a href='$link_address1'>Main Menu</a>";
 
@@ -38,14 +39,10 @@ echo "<a href='$link_address4'>Delete a Movie</</a>";
 $link_address5 = 'Rate.php';
 echo "<a href='$link_address5'>Rate a Movie</a>";
 
-
-
-echo '</html>';
-
 ?>
-<h2>Welcome to MP Movie House</h2>
 
 <div class="sidenav">
+ 
 <a href='$link_address1'>Main Menu</a>
 
 <a href='$link_address2'>Search for Movies</a>
@@ -58,15 +55,19 @@ echo '</html>';
 
 </div>
 
+<h2>MP Movie House - Main Menu</h2>
 
-<br \>
-<textarea name="Menu Welcome" rows=10 cols=80>
+<div class="wrapper">
 <h3>Welcome to MP Movie House</h3>
+
 <br/>Keep Inventory of all your movies with this simple application.
+
 <br/>Just enter the Title, Release Years, Director, Actors, and Genre.
+
 <br/>Search and Rate your movies too!
+
 Enjoy!
 <br/>
-</textarea>
-<br/>
-
+</div>
+</body>
+  </html>
